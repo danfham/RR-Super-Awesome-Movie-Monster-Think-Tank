2787a87d7f79f0f03bbf96e80c46251b85d8fb38
danfham <dan.cunningham91@icloud.com>
2023-08-16 19:11:20 -0400

    implemented lazy load functionality with a sweet little puff img associated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Puff} from 'react-loader-spinner'
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
 
 function App() {
+  const AboutUs = React.lazy(()=> import('./components/AboutUs'))
+  const Packages = React.lazy(() => import('./components/Packages'))
+
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense fallback={<Puff 
+            type="Bars" 
+            color="#FF00FF" 
+            height={200} 
+            width={200} 
+            style={{margin: "0 auto"}}/>}>
           <AboutUs />
           <Packages />
+        </Suspense>
+
       </div>
     </div>
   );
